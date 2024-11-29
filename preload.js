76089f58b657621ba67f8c1d2fbfa96bb1b49343
Max Ward <maxward3@outlook.com>
2024-11-29 23:12:14 +0000

    Remove pre-load

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  // Add specific APIs you want to expose to the renderer process
-  sayHello: () => console.log('Hello from Electron!'),
-});
